6126d6ae399bc6e88e94a7d9a3094ce20f129fcc
WhyWhiteee <nord3king@gmail.com>
2024-11-24 19:57:28 +0300

    доделать вывод по курсе и перевод на новый курс

--- src/pckg/Main.java
+++ src/pckg/Main.java
@@ -1,4 +1,25 @@
 package pckg;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        var diana = new Student("Диана", "24-ИСТ", 1, new int[]{5, 4, 5, 3, 5});
+        var alex = new Student("Александр", "24-ИСТ", 1, new int[]{4, 3, 5, 3, 4});
+        var vlad = new Student("Владислав", "23-ИВТ", 2, new int[]{2, 3, 2, 3, 2});
+        var mary = new Student("Мария", "23-ИВТ", 2, new int[]{5, 5, 5, 5, 5});
+        var daniil = new Student("Даниил", "22-МОА", 3, new int[]{2, 3, 2, 3, 3});
+        var michael = new Student("Михаил", "22-МОА", 3, new int[]{4, 3, 4, 3, 4});
+
+        List<Student> students = new ArrayList<>(Arrays.asList(diana, alex, vlad, mary, daniil, michael));
+
+        System.out.println(students.size());
+
+        Student.deleteStudents(students);
+
+        System.out.println(students.size());
+
+    }
 }
--- src/pckg/Student.java
+++ src/pckg/Student.java
@@ -1,4 +1,35 @@
 package pckg;
 
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Student {
+    private String name;
+    private String group;
+    private int course;
+    private int[] grades;
+
+    public Student(String name, String group, int course, int[] grades) {
+        this.name = name;
+        this.group = group;
+        this.course = course;
+        this.grades = grades;
+    }
+
+    public static void deleteStudents(List<Student> students) {
+        Iterator<Student> iterator = students.iterator();
+        while (iterator.hasNext()) {
+            Student nextStudent = iterator.next();
+            int avgGrade = 0;
+
+            for (int grade : nextStudent.grades)
+                avgGrade += grade;
+
+            if (avgGrade / nextStudent.grades.length < 3)
+                iterator.remove();
+        }
+    }
+
 }
